f015d6b6b611e93fbc20ef8fb417f87dfafb6e74
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-19 20:40:24 +0200

    completed task 3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring> 
 using namespace std;
 
 int main() {
-	return 0;
+    char str[] = "Series of Darksouls, Sekiro, Blodborne are soulslike games";
+    char* word = strtok(str, " ");
+    int wordCount = 0;
+    while (word != nullptr) {
+        wordCount++;
+        word = strtok(nullptr, " ");
+    }
+    cout << "Number of words: " << wordCount << endl;
+    
+    return 0;
 }
-
