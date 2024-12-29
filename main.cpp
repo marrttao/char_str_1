70a7eadbc5fc3b3aeab80574d1cdae0ee3709131
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-29 15:32:43 +0200

    completed task 4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
 
 int main() {
-	return 0;
-}
+    char* str = new char[100];
+    strcpy(str, "Text with dots. To replace them into !. . . .");
+    cout << str << endl;
 
+    char* temp = new char[100];
+    int j = 0;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == '.') {
+            temp[j] = '!';
+            j++;
+        }
+        else {
+            temp[j] = str[i];
+            j++;
+        }
+    }
+    temp[j] = '\0';
+    strcpy(str, temp);
+    cout << str << endl;
+    delete[] temp;
+    delete[] str;
+
+    return 0;
+}
