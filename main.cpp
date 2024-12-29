d47d852d2725a0da27d866703e67eeafd46e7b3e
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-29 13:29:02 +0200

    first version

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
-	return 0;
-}
+    char str[] = "LoLll";
+    char reverse[sizeof(str)];
+    int j = 0;
+    for (int i = strlen(str) - 1; i >= 0; i--) {
+        reverse[j] = str[i];
+        j++;
+    }
+    reverse[j] = '\0';
 
+    if (strcmp(str, reverse) == 0) {
+        cout << "Palindrom";
+    }
+    else {
+        cout << "Not palindrom";
+    }
+    return 0;
+}
