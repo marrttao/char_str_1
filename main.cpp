6292e2a03ee639612c42f7d57c617b4f5cce4fce
Maris Shardyko <marrttaoburberry@gmail.com>
2024-12-29 15:28:12 +0200

    completed task 3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
 
-int main() {
-	return 0;
+void paste_in_index(char* str, int* index, char* symbol) {
+    if (*index < 0) {
+        cout << "Index is too small, using minimum index (0)." << endl;
+        *index = 0;
+    }
+
+    size_t len = strlen(str);
+    if (*index > static_cast<int>(len)) {
+        cout << "Index is too large, using maximum index (" << len << ")." << endl;
+        *index = len;
+    }
+    char* temp = new char[len + 2];
+
+
+    size_t j = 0;
+    for (size_t i = 0; i <= len; ++i) {
+        if (static_cast<int>(i) == *index) {
+            temp[j++] = *symbol;
+        }
+        if (i < len) {
+            temp[j++] = str[i];
+        }
+    }
+    temp[j] = '\0';
+
+    strcpy(str, temp);
+    delete[] temp;
 }
 
+
+int main() {
+    char* str = new char[100];
+    strcpy(str, "Paste symbol in index");
+    cout << str << endl;
+    int* index = new int;
+    char* symbol = new char;
+
+    cout << "Enter index: ";
+    cin >> *index;
+    cout << "Enter symbol: ";
+    cin >> *symbol;
+
+    paste_in_index(str, index, symbol);
+
+    cout << str << endl;
+
+    delete[] str;
+    delete index;
+    delete symbol;
+
+    return 0;
+}
